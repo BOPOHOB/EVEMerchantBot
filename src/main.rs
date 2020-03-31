45af419909d9b9864d620ecfe161c8d453c1f152
BOPOHOB <self@bopohob.ru>
2020-03-31 14:13:14 +0300

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+extern crate dotenv;
 use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
     run().await;
 }
 
@@ -21,7 +23,8 @@ async fn run() {
         .messages_handler(|rx: DispatcherHandlerRx<Message>| {
             rx.for_each(|message| async move {
                 let eve_client_id: String = get_env_variable("EVE_CLIENT_ID");
-                let url = format!("https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=http://localhost:8000/&state={}&client_id={}&scope=esi-markets.structure_markets.v1", message.update.chat_id(), eve_client_id);
+                let redirect_uri = get_env_variable("REDIRECT_URI");
+                let url = format!("https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri={}&state={}&client_id={}&scope=esi-markets.structure_markets.v1", redirect_uri, message.update.chat_id(), eve_client_id);
                 let text = format!("Привет. Чтобы бот мог следить за ситуацией на рынке позволь ему получать информацию из магазина от твоего лица. Для этого перейди по [ссылке]({}) и разреши приложению авторизоваться", url);
                 message.answer(text).parse_mode(teloxide::types::ParseMode::Markdown).send().await.log_on_error().await;
             })
