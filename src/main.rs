abfafa798b2adf0ec2bcabaaf0445b627243b697
BOPOHOB <self@bopohob.ru>
2020-04-02 22:16:30 +0300

    change required permission

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ async fn run() {
             rx.for_each(|message| async move {
                 let eve_client_id: String = get_env_variable("EVE_CLIENT_ID");
                 let redirect_uri = get_env_variable("REDIRECT_URI");
-                let url = format!("https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri={}&state={}&client_id={}&scope=esi-markets.structure_markets.v1", redirect_uri, message.update.chat_id(), eve_client_id);
+                let url = format!("https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri={}&state={}&client_id={}&scope=esi-markets.read_character_orders.v1", redirect_uri, message.update.chat_id(), eve_client_id);
                 let text = format!("Привет. Чтобы бот мог следить за ситуацией на рынке позволь ему получать информацию из магазина от твоего лица. Для этого перейди по [ссылке]({}) и разреши приложению авторизоваться", url);
                 message.answer(text).parse_mode(teloxide::types::ParseMode::Markdown).send().await.log_on_error().await;
             })
